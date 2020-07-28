0d2c48a9ee2db4bb7ffd3c3f4355e3fdb544aa8b
grantkoji <grantkoji@gmail.com>
2020-07-28 18:23:03 -0400

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,32 @@
 export function manageFriends(state, action){
+   switch (action.type) {
+       case 'ADD_FRIEND':
+        //    let newFriends = Object.assign({}, state.friends)
+           return {friends: [...state.friends, action.friend]}
+        case 'REMOVE_FRIEND':
+            return {friends: [...state.friends].filter(friend => friend.id !== action.id)};
+        default: 
+            return state;
+   }
 }
+
+
+
+// function changeState(state, action){      
+//     switch (action.type) {
+//       case 'INCREASE_COUNT':
+//         return {count: state.count + 1}
+//       case 'DECREASE_COUNT':
+//         return {count: state.count - 1}
+//       default:
+//         return state;
+//     }
+//   }
+   
+//   let state = {count: 0}
+   
+//   changeState(state, {type: 'INCREASE_COUNT'})
+//   // => {count: 1}
+   
+//   changeState(state, {type: 'DECREASE_COUNT'})
+//   // => {count: -1}
